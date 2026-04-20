1d194a3eb7beb756136af83bfa26e196e597e604
2024125046 <emily52230@gmail.com>
2026-04-20 15:47:45 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int answer, a, count = 0;
+    scanf("%d", &answer);
+
+    do {
+        count ++;
+        scanf("%d", &a);
+        if (a != answer){
+            if (a < answer){
+            printf("%d<?\n", a);
+            } else {
+            printf("%d>?\n", a);
+            }
+        } else{
+            printf("%d==?\n", a);
+        }
+    } while(a!=answer);
+
+    printf("%d", count);
+        
+    return 0;
+}
+
