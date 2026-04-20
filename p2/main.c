d3798b0d9154cfd31fc4cec96d249740fe39e37b
2024125046 <emily52230@gmail.com>
2026-04-20 15:53:29 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main() {
+    int N;
+    int eng = 0, num = 0;
+    int maxEng = 0, maxNum = 0;
+    char a[100];
+    scanf("%d", &N);
+    scanf("%s", a);
+
+    for (int i = 0; i < N; i++) {
+        char ch = a[i];
+        if (ch >= 'a' && ch <= 'z') {
+            eng++;
+            num = 0;
+            if (eng > maxEng) maxEng = eng;
+        } 
+        else if (ch >= '0' && ch <= '9') {
+            num++;         
+            eng = 0;
+            if (num > maxNum) maxNum = num;
+        } 
+        else {
+            eng = 0;
+            num = 0;
+        }
+    }
+    printf("%d\n", maxEng);
+    printf("%d\n", maxNum);
+
+    return 0;
+}
